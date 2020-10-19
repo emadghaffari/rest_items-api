15128abf402b8aeaf2f831861c101cfa9a224bd2
Emad Ghaffari <emadghaffariii@gmail.com>
2020-10-19 16:35:16 +0000

    add services folder

--- services/item_services.go
+++ services/item_services.go
@@ -1,2 +1,26 @@
 package services
 
+import (
+	"github.com/emadghaffari/res_errors/errors"
+	"github.com/emadghaffari/rest_items-api/domain/items"
+)
+
+var (
+	// ItemService var from itemsServiceInterface interface
+	ItemService itemsServiceInterface = &itemService{}
+)
+
+type itemsServiceInterface interface {
+	Get(string) (*items.Item, *errors.ResError)
+	Create(*items.Item) (*items.Item, *errors.ResError)
+}
+
+type itemService struct{}
+
+func (s *itemService) Get(string) (*items.Item, *errors.ResError) {
+	return nil, errors.HandlerBadRequest("implement me")
+}
+
+func (s *itemService) Create(*items.Item) (*items.Item, *errors.ResError) {
+	return nil, errors.HandlerBadRequest("implement me")
+}
