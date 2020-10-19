14a5f0e66c25a57923b220ad724757df3f7a7d56
Emad Ghaffari <emadghaffariii@gmail.com>
2020-10-19 16:35:26 +0000

    add domains folder

--- domain/items/item.go
+++ domain/items/item.go
@@ -1,26 +1,27 @@
 package items
 
 // Item struct
-type Item struct{
-	ID int64 `json:"id"`
-	Saller string `json:"saller"`
-	Title string `json:"title"`
-	Description Description `json:"description"`
-	Pictures []Picture `json:"pictures"`
-	Video string `json:"video"`
-	Price float32 `json:"price"`
-	AvailableQuantity int `json:"available_quantity"`
-	SoldQuantity int `json:"sold_quantity"`
-	Status string `json:"status"`
+type Item struct {
+	ID                int64       `json:"id"`
+	Seller            int64       `json:"seller"`
+	Title             string      `json:"title"`
+	Description       Description `json:"description"`
+	Pictures          []Picture   `json:"pictures"`
+	Video             string      `json:"video"`
+	Price             float32     `json:"price"`
+	AvailableQuantity int         `json:"available_quantity"`
+	SoldQuantity      int         `json:"sold_quantity"`
+	Status            string      `json:"status"`
 }
+
 // Description struct
-type Description struct{
+type Description struct {
 	PlainText string `json:"plain_text"`
-	HTML string `json:"html"`
+	HTML      string `json:"html"`
 }
 
 // Picture struct
-type Picture struct{
-	ID int64 `json:"id"`
+type Picture struct {
+	ID  int64 `json:"id"`
 	URL int64 `json:"url"`
 }
